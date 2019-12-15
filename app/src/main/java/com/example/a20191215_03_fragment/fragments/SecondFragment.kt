adcb02e8c4248c2a6a819b537c90f6e4be0cab4d
gyucode <gyucode@gmail.com>
2019-12-15 16:09:46 +0900

    두번째 프래그먼트 간략하게 생성

---
+++ app/src/main/java/com/example/a20191215_03_fragment/fragments/SecondFragment.kt
@@ -0,0 +1,34 @@
+package com.example.a20191215_03_fragment.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.a20191215_03_fragment.R
+
+class SecondFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
